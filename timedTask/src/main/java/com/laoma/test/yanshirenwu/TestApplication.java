b1424ced96dcd4ed098e0a6146be86914a0268e2
tonyma91 <tony_ma91@126.com>
2021-05-08 17:13:40 +0800

    拉下来测试一下，出定时任务因为版本的原因无法运行以外，其余均正常

--- timedTask/src/main/java/com/laoma/test/yanshirenwu/TestApplication.java
+++ timedTask/src/main/java/com/laoma/test/yanshirenwu/TestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
   * @description 主启动类，会自动扫描延时任务
+ * //TODO 因为是直接进行的复制，所以 此时无法直接 运行，原因是因为多模块服务，子模块与父模块的版本不同
   * @author majinming@xiaomi.com
   * @date 2021年 03月19日 10:50
   */
